ddc38867209a5d2fb609e26f056c38bd389d1110
Schilkroete <schilkroete@gmail.com>
2016-12-29 21:16:42 +0100

    BraunTrainer, Text verändert

--- app/src/main/java/schilkroete/sammlung/Reaktion.java
+++ app/src/main/java/schilkroete/sammlung/Reaktion.java
@@ -15,7 +15,8 @@ import java.util.logging.Logger;
  * Created by Schilkroete on 29.12.2016.
  */
 
-public class Reaktion extends AppCompatActivity implements Runnable, View.OnTouchListener, View.OnClickListener{
+public class Reaktion extends AppCompatActivity
+        implements Runnable, View.OnTouchListener, View.OnClickListener{
 
     TextView tv_now, tv_time;
     Button btn_start;
@@ -44,16 +45,58 @@ public class Reaktion extends AppCompatActivity implements Runnable, View.OnTouc
 
 
 
-    public void onClick(View view){
-        // den Moment, berechnen, wann der User reagieren soll, also eine Zufallszahl
-        reactionMoment = ((Math.random()) * 6 + 2);
-        running = true;
-        // Neuer Thread soll gestartet werden, dieser soll eine Berechnung vornehmen
-        thread_count = new Thread(this);
-        thread_count.start();
-        // Der Button soll gesperrt werden, damit dieser nicht mehr angeklickt werden kann
-        btn_start.setClickable(false);
+    /* Berechnung im Hintergrund
+  *
+  * hier wird die Zeit gezählt und wenn der Zeitpunkt erreicht ist, wenn User reagieren
+  * soll (reactionMoment) dann werden die entsprechenden Operationen in die Wege geleitet.
+  * Anschließend  zählt der Thread die Zeit, wie lange der User benötigt, um zu reagieren
+  */
+    public void run(){
+        time = 0;
+        reactionTime = 0;
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                tv_now.setText("Warte!");
+                tv_now.setVisibility(View.VISIBLE);
+                tv_time.setText(String.format("%.2f", reactionTime));
+            }
+        });
+
+        while (running) {
+            time = time + 0.01;
+            if (time == reactionMoment) {
+                reactNow = true;
+            }
+            if (reactNow) {
+                reactionTime += 0.01;
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        tv_now.setText("Reagiere!");
+                        tv_time.setText(String.format("%.2f", reactionTime));
+                    }
+                });
+            }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Reaktion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        runOnUiThread(new Runnable() {
+            public void run() {
+                tv_now.setVisibility(View.INVISIBLE);
+            }
+        });
     }
+    /* Je nachdem in welchem Zustand sich der Reaktionstest befindet, ändern wir den Text unseres
+    * TV's entweder zu "Warte!" oder "Reagiere!". So weiß der User sofort, wenn er reagieren soll,
+    * nämmlich sobald der Text auf "Reagiere!" umspringt. Berührt er dann den Bildschirm der App,
+    * wird die Zeit gestoppt
+    */
+
+
+
 
 
     // was passiert, wenn der User den Bildschirm berührt
@@ -76,58 +119,19 @@ public class Reaktion extends AppCompatActivity implements Runnable, View.OnTouc
     }
 
 
-    /* Berechnung im Hintergrund
-    *
-    * hier wird die Zeit gezählt und wenn der Zeitpunkt erreicht ist, wenn User reagieren
-    * soll (reactionMoment) dann werden die entsprechenden Operationen in die Wege geleitet.
-    * Anschließend  zählt der Thread die Zeit, wie lange der User benötigt, um zu reagieren
-    */
-    public void run(){
-        time = 0;
-        reactionTime = 0;
-
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                tv_now.setText("Warte!");
-                tv_now.setVisibility(View.VISIBLE);
-                tv_time.setText(String.format("%.2f", reactionTime));
-            }
-        });
-
-        while (running) {
-            time = time + 0.01;
-            if (time == reactionMoment) {
-                reactNow = true;
-            }
-            if (reactNow) {
-                reactionTime += 0.01;
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        tv_now.setText("Reagiere!");
-                        tv_now.setText(String.format("%.2f", reactionTime));
-                    }
-                });
-            }
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Reaktion.class.getName()).log(Level.SEVERE, null, ex);
-            }
 
 
-        }
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                tv_now.setVisibility(View.INVISIBLE);
-            }
-        });
+    public void onClick(View view){
+        // den Moment, berechnen, wann der User reagieren soll, also eine Zufallszahl
+        reactionMoment = ((Math.random()) * 6 + 2);
+        running = true;
+        // Neuer Thread soll gestartet werden, dieser soll eine Berechnung vornehmen
+        thread_count = new Thread (this);
+        thread_count.start();
+        // Der Button soll gesperrt werden, damit dieser nicht mehr angeklickt werden kann
+        btn_start.setClickable(false);
     }
-    /* Je nachdem in welchem Zustand sich der Reaktionstest befindet, ändern wir den Text unseres
-    * TV's entweder zu "Warte!" oder "Reagiere!". So weiß der User sofort, wenn er reagieren soll,
-    * nämmlich sobald der Text auf "Reagiere!" umspringt. Berührt er dann den Bildschirm der App,
-    * wird die Zeit gestoppt
-    */
+
+
+
 }
